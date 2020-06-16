a1f09f8f83ccffe766cc5d695ecec3af743e66cf
TheGrandestNothing <zhilyaev.slava@mail.ru>
2020-06-16 18:19:25 +0500

    RegisterTEst1

--- LoginLogic/User.cs
+++ LoginLogic/User.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Wiki.LoginLogic
 {
@@ -14,14 +16,22 @@ namespace Wiki.LoginLogic
 
         public static bool LoginUser(string login, string password)
         {
-
+            return false;
 
         }
 
         public static bool RegisterUser(string login, string password, string secondPassword)
         {
+            if()
             if (password != secondPassword) return false;
-            // нет такого логина
+
+            var exApp = new Excel.Application();
+            var xlWb = exApp.Workbooks.Open("\\..\\..\\Users.xlsx");
+            var exWrkSht = xlWb.Sheets[1];
+            var firstEmpty = exWrkSht.Cells[exWrkSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row + 1;
+
+            for (var i = 0; i < firstEmpty; i++)
+                if (login == exWrkSht.Cells[i, 0]) return false;
 
             var bytes = Encoding.Unicode.GetBytes(password);
             var CSP = new MD5CryptoServiceProvider();
@@ -30,7 +40,12 @@ namespace Wiki.LoginLogic
             foreach (byte b in byteHash)
                 hash += string.Format("{0:x2}", b);
             Password = hash;
-            // запись в эксель
+
+            exWrkSht.Cells[firstEmpty, 0] = login;
+            exWrkSht.Cells[firstEmpty, 1] = hash;
+            xlWb.Close(true);
+            exApp.Quit();
+
             return true;
         }
     }
