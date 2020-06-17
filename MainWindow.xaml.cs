842ea64b47dd3aead2514924f3a9e1753ab47e4d
Kirill Kovalkov <kowalkof@yandex.ru>
2020-06-17 15:35:04 +0500

    основное окно

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,7 +22,82 @@ namespace Wiki
     {
         public MainWindow()
         {
+            var articles = new List<string>() {"123", "12", "1"};
+
             InitializeComponent();
+
+            layoutGrid.Background = Brushes.White;
+            layoutGrid.ShowGridLines = true;
+
+            var leftCol = new ColumnDefinition();
+            leftCol.Width = new GridLength(300, GridUnitType.Pixel);
+            var rightCol = new ColumnDefinition();
+            rightCol.Width = new GridLength(1,GridUnitType.Star);
+
+            layoutGrid.ColumnDefinitions.Add(leftCol);
+            layoutGrid.ColumnDefinitions.Add(rightCol);
+
+            var leftStackPanel = new StackPanel();
+            leftStackPanel.HorizontalAlignment = HorizontalAlignment.Left;
+            leftStackPanel.VerticalAlignment = VerticalAlignment.Top;
+            Grid.SetColumn(leftStackPanel, 0);
+            Grid.SetRow(leftStackPanel, 0);
+
+            var searchTextBox = new TextBox();
+            searchTextBox.Margin = new Thickness(0, 0, 0, 990);
+            searchTextBox.FontSize = 18;
+            searchTextBox.HorizontalAlignment = HorizontalAlignment.Stretch;
+            searchTextBox.Text = "Search";
+            searchTextBox.Foreground = Brushes.Gray;
+            searchTextBox.GotKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_GotKeyboardFocus);
+            searchTextBox.LostKeyboardFocus += new KeyboardFocusChangedEventHandler(tb_LostKeyboardFocus);
+
+            var listElement = new DataTemplate();
+            listElement.DataType = typeof(string);
+            FrameworkElementFactory spFactory = new FrameworkElementFactory(typeof(StackPanel));
+            spFactory.Name = "myComboFactory";
+            spFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+            FrameworkElementFactory cardHolder = new FrameworkElementFactory(typeof(TextBlock));
+            cardHolder.SetBinding(TextBlock.TextProperty, new Binding("Title"));
+            cardHolder.SetValue(TextBlock.ToolTipProperty, "Title");
+            spFactory.AppendChild(cardHolder);
+            listElement.VisualTree = spFactory;
+            
+            var listBox = new ListBox();
+            listBox.HorizontalAlignment = HorizontalAlignment.Stretch;
+            listBox.Margin = new Thickness(0);
+            listBox.ItemsSource = articles;
+            listBox.ItemTemplate = listElement;
+
+            leftStackPanel.Children.Add(searchTextBox);
+            leftStackPanel.Children.Add(listBox);
+
+            layoutGrid.Children.Add(leftStackPanel);
+        }
+
+        private void tb_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                //If nothing has been entered yet.
+                if (((TextBox) sender).Foreground == Brushes.Gray)
+                {
+                    ((TextBox) sender).Text = "";
+                    ((TextBox) sender).Foreground = Brushes.Black;
+                }
+            }
+        }
+
+        private void tb_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                if (((TextBox)sender).Text.Trim().Equals(""))
+                {
+                    ((TextBox)sender).Foreground = Brushes.Gray;
+                    ((TextBox)sender).Text = "Text";
+                }
+            }
         }
     }
 }
