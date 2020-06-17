61fc4e3182f35b0473418637f750f1c29a50e135
TheGrandestNothing <zhilyaev.slava@mail.ru>
2020-06-17 16:05:31 +0500

    ArticleLogic done

---
+++ ArticleLogic/Article.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiki.ArticleLogic
+{
+    class Article
+    {
+        public string Header;
+        public string Content;
+
+
+        public static Article CreateArticle(string fileName)
+        {
+            var article = new Article();
+            var sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + fileName);
+            var str = sr.ReadToEnd().Split('\'');
+            article.Header = str.First();
+            article.Content = str.Last();
+            return article;
+        }
+    }
+}
